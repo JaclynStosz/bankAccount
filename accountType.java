74d50252bb6173780d35af65ee9ad968d0380b87
Jaclyn Stosz <45833294+JaclynStosz@users.noreply.github.com>
2020-08-15 16:28:00 -0700

    Update accountType.java

--- accountType.java
+++ accountType.java
@@ -1,35 +1,47 @@
 /*
-Parent class for account types
+Parent class for handling common account functions
 */
+
 public class accountType
 {
-
+    private String name:
     private double balance;
     private int minOpen;
     private double interest;
 
     public accountType()
     {
+        name = "";
         balance = 0;
         minOpen = 0;
         interest = 0;
     }
-
-    public accountType(double bal, double min, double in)
-    {
-        balance = bal;
-        minOpen = min;
-        interest = in;
+    
+    public void setName(String n){
+        name = n   
     }
     
-    /*
     public void setMinOpen(int min){
-           
+          minOpen = min; 
     }
 
     public void setInterest(double in){
            interest = in;
     }
-    */
+    
+    public void deposit(double dep){
+        balance = balance + dep;
+    }
+    
+    public void withdraw(double wit){
+        if (wit < balance)
+        {
+            balance = balance - wit;   
+        }
+        else
+        {
+            //send insufficient funds error   
+        }
+    }
     
 }
