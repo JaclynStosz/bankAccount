eecc0f9f9b307207dd20b29859c0e83b48518a0d
Jaclyn Stosz <45833294+JaclynStosz@users.noreply.github.com>
2020-08-15 16:34:52 -0700

    Update checking.java

--- checking.java
+++ checking.java
@@ -1,7 +1,16 @@
 
 private class checking extends accountType
 {
-
+    public int transferLimit; //num of allotted monthly online transfers
+    public boolean autoTransfer; //will money be transfered each month
+    public double autoAmount; //amount transfered monthly
     
+    public checking(){
+        minOpen = 100;
+        interest = 0.01;
+        transferLimit = 10;
+        autoTransfer = false;
+        autoAmount = 0;
+    }
 
 }
